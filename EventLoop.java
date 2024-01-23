67ed759efbc66b82c9fa88f0bfe15253b28df8f1
Caden Phan <caden.phan@mrjaffesclass.com>
2024-01-23 11:41:50 -0800

    Initial Commit

---
+++ EventLoop.java
@@ -0,0 +1,46 @@
+ public class EventLoop {
+
+  // Instance variables for the UI and State classes
+  State state = new State();
+  UI ui = new UI();
+  int row, col;
+
+  public void eventLoop() {
+    while (state.getGameState() != Constants.QUIT_PROGRAM) {
+      int gameState = state.getGameState();
+      if (gameState == Constants.STANDBY) {
+        state.setGameState(Constants.GET_X_NAME);
+
+      } else if (gameState == Constants.GET_X_NAME) {
+        state.setXName(ui.promptForName(Constants.X));
+        state.setGameState(Constants.GET_O_NAME);
+    
+      } else if (gameState == Constants.GET_O_NAME) {
+        state.setOName(ui.promptForName(Constants.O));
+        state.setGameState(Constants.GET_X_MOVE);
+    
+      } else if (gameState == Constants.GET_X_MOVE) {
+        row = ui.getMoveRow(state.getWhoseMove());
+        col = ui.getMoveCol(state.getWhoseMove());
+        if (ui.isLegalMove(state, row, col)) {
+          state.setGameState(Constants.MAKE_MOVE);
+        }
+
+      } else if (gameState == Constants.GET_O_MOVE) {
+        row = ui.getMoveRow(state.getWhoseMove());
+        col = ui.getMoveCol(state.getWhoseMove());
+        if (ui.isLegalMove(state, row, col)) {
+          state.setGameState(Constants.MAKE_MOVE);
+        }
+
+      } else if (gameState == Constants.MAKE_MOVE) {
+        ui.printMove(state, row, col);
+        state.setBoardCell(state.getWhoseMove(), row, col);
+        state.setGameState(Constants.CHECK_IF_WINNER);
+
+      } else if (gameState == Constants.CHECK_IF_WINNER) {
+        if (state.isWinner()) {
+          if (state.getWhoseMove() == Constants.X) {
+            state.setGameState(Constants.X_WINS);
+          } else {
+            state.setGameState(Constants.O_WINS);
