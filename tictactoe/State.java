11638a54d073c96127ed43b9c1d81c193e478667
Caden Phan <caden.phan@mrjaffesclass.com>
2024-02-02 11:38:25 -0800

    Initial commit

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -9,7 +9,7 @@ public class State
     private String xName = "";
     private String oName = "";
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
-
+    
     public boolean isWinner() {
         // You will write this code!!
     }
@@ -17,19 +17,43 @@ public class State
     public boolean isTie() {
         // You will write this code too!!
     }
-    
+
     public int getGameState() {
         return this.gameState;
     }
-    
+
     public void setGameState(int gameState) {
         this.gameState = gameState;
+    }
+
+    public int getWhoseMove() {
+        return whoseMove;
+    }
+
+    public void setWhoseMove(int whoseMove) {
+        this.whoseMove = whoseMove;
+    }
+    
+    public String getXName() {
+        return xName;
+    }
+    
+    public String getOName() {
+        return oName;
+    }
+    
+    public void setOName(String oName) {
+        this.oName = oName;
+    }
+    
+    public void setXName(String xName) {
+        this.xName = xName;
     }
     
     public int getBoardCell(int row, int col) {
         return this.board[row][col];
     }
-    
+
     public void setboardCell(int row, int col, int value) {
         this.board[row][col] = value;
     }
